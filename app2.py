ea71dee82fc19e6a8b9b3fe643d901e4b129b21d
beaming <132054647+beamingnp@users.noreply.github.com>
2024-07-12 11:00:19 +0330

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, send_file, request
+import openpyxl
+from collections import defaultdict
+
+app = Flask(__name__)
+
+# Load the Excel file once when the app starts
+workbook = openpyxl.load_workbook('C:\\Users\\Asus\\Desktop\\term 6 exam\\test2\\pregnancy1.xlsx')
+sheet = workbook.active
+data = defaultdict(str)
+
+# Preprocess the Excel data for faster searching
+for row in range(2, sheet.max_row + 1):
+    code = sheet.cell(row=row, column=1).value
+    explanation = sheet.cell(row=row, column=2).value
+    if code is not None and explanation is not None:
+        if code not in data:
+            data[code] = ""
+        if data[code]:
+            data[code] += "\n\n"
+        data[code] += explanation
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/download_pdf')
+def download_pdf():
+    return send_file('C:\\Users\\Asus\\Desktop\\term 6 exam\\test2\\book-3-pregnancy.pdf', as_attachment=True)
+
+@app.route('/search_code', methods=['POST'])
+def search_code():
+    search_term = request.form['search_term'].upper()
+    if search_term in data:
+        results = [{"code": search_term, "explanation": data[search_term]}]
+    else:
+        results = []
+    return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
